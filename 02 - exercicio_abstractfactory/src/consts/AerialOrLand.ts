8629eb8500e94c207a3fe0ad20418de848c6a082
SpecterInAHusk <83662671+SpecterInAHusk@users.noreply.github.com>
2023-06-20 14:42:15 -0300

    remover pasta antiga renomeada

--- 02 - exercicio_abstractfactory/src/consts/AerialOrLand.ts
+++
@@ -1,6 +0,0 @@
-//const para escolher se o veículo é áereo ou terrestre
-
-export default {
-    AIR: "AIR",
-    LAND: "LAND"
-}
--- 02 - exercicio_abstractfactory/src/consts/Company.ts
+++
@@ -1,7 +0,0 @@
-//Constantes para escolher se é 99 ou Uber
-
-export default {
-    UBER: "UBER",
-    NINENINE: "NINENINE",
-    LIME: "LIME"
-}
